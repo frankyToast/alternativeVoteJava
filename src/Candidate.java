448574180b8b5c87fa1cba63aa3a48a4b5fa6817
frankyToast <94389782+frankyToast@users.noreply.github.com>
2023-01-18 11:53:33 -0500

    Adding project onto GitHub via terminal

--- src/Candidate.java
+++ src/Candidate.java
@@ -1,6 +1,9 @@
 import java.util.*;
-
+/**
+ * @author Franky Yang
+ */
 public class Candidate{
+    
     private String name;
     private int pos;
 
@@ -11,26 +14,46 @@ public class Candidate{
         this.pos = p;
     }
 
+    /**
+     * 
+     * @param name description
+     */
     public void changeName(String name){
         this.name = name;
     }
- 
+
+    /**
+     * 
+     * @return asldkfasl
+     */
     public String getName(){
         return name;
     }
 
+    /**
+     *WHAT METHOD DOES
+     * @return descrualdjla
+     */
     public int getNumVotes(){
         return candidateVotes.size();
     }
-    
+    /**
+     * 
+     * @return and integer desclkj
+     */
     public int getPOS(){
         return pos;
     }
-    
+    /**
+     * 
+     * @param vote
+     */
     public void addVote(Ballot vote){
         candidateVotes.add(vote);
     }
-    
+    /**
+     * 
+     */
     public void clear(){
         candidateVotes.clear();
     }
